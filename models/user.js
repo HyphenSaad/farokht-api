361022cd27c04c76797929538fb44ba845da06c2
Saadin <hyphensaad@gmail.com>
2023-02-11 00:26:09 +0500

    axhwwwwww

--- models/user.js
+++ models/user.js
@@ -59,7 +59,6 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Password is required!'],
     minLength: [8, 'Password is too short!'],
-    maxLength: [50, 'Password is too long!'],
     match: [/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/,
       'Password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 digit!'],
     select: false, // TODO: Yeh Kya Karta Hai???
