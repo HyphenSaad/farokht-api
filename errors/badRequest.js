a462b91704772cbb1928101e22cd93f43381b27f
Saadin <hyphensaad@gmail.com>
2023-02-10 18:31:04 +0500

    as

--- errors/badRequest.js
+++ errors/badRequest.js
@@ -4,6 +4,7 @@ import CustomAPIError from './CustomAPIError.js'
 class BadRequestError extends CustomAPIError {
   constructor(message) {
     super(message)
+    Error.captureStackTrace(this, this.constructor)
     this.statusCode = StatusCodes.BAD_REQUEST
   }
 }
