fbce8d91a992569ac27ad0884943d1add619ca9f
Saadin <hyphensaad@gmail.com>
2023-02-10 22:38:58 +0500

    abhi fit ho gya hai

--- errors/index.js
+++
@@ -1,5 +0,0 @@
-// import BadRequestError from './badRequest.js'
-// import NotFoundError from './notFound.js'
-// import UnAuthorizedError from './unAuthorized.js'
-
-// export { BadRequestError, NotFoundError, UnAuthorizedError }
