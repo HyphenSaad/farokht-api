0c9baf23ac9b4a0652a51974758f6498a8785444
Saadin <hyphensaad@gmail.com>
2023-02-10 17:38:09 +0500

    axha

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import ConnectMongoDB from './configs/database.js'
 // ROUTERS
 // import Router from './routes/index.js'
 // MIDDLEWARES
-import { NotFoundMiddleware, ErrorHandlerMiddleware } from './middlewares/index.js'
+import { NotFoundMiddleware } from './middlewares/index.js'
 
 const app = express()
 const PORT = process.env.PORT || 4000
