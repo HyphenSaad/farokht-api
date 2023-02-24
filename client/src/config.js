cb241cb8b8bf37ebef6299499695345c39d4f9c4
Saadin <hyphensaad@gmail.com>
2023-02-24 20:13:04 +0500

    ...

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-// export const API_BASE_URL = 'http://localhost:5000/api/v1/'
-export const API_BASE_URL = 'https://prickly-lime-pigeon.cyclic.app/api/v1/'
+export const API_BASE_URL = 'http://localhost:5000/api/v1/'
+// export const API_BASE_URL = 'https://prickly-lime-pigeon.cyclic.app/api/v1/'
 export const APP_TITLE = 'Admin Panel | Farokht'
