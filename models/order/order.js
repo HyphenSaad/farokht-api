f6bfd02ec0fe6dee1f1a7bf9c0bdc83897793df6
Khalil Ul Rehman <khalil.urrehman85@gmail.com>
2023-03-11 16:48:43 +0500

    OrderSchemaAdded

--- models/order/order.js
+++ models/order/order.js
@@ -4,20 +4,20 @@ import { User, Item, AttributeOfItem } from '../index.js'
 const notEmpty = (array) => array.length !== 0
 
 const OrderSchema = new mongoose.Schema({
-  vendorId: {
+  itemId: {
     type: Schema.Types.ObjectId,
-    ref: User,
-    required: [true, 'Vendor ID is required!'],
+    ref: Item,
+    required: [true, 'Item ID is required!'],
   },
   retailerId: {
     type: Schema.Types.ObjectId,
     ref: User,
     required: [true, 'Retailer ID is required!'],
   },
-  itemId: {
+  vendorId: {
     type: Schema.Types.ObjectId,
-    ref: Item,
-    required: [true, 'Item ID is required!'],
+    ref: User,
+    required: [true, 'Vendor ID is required!'],
   },
   attributes: {
     type: [{
@@ -32,7 +32,7 @@ const OrderSchema = new mongoose.Schema({
     }],
     validate: [notEmpty, 'Attributes are required!'],
   },
-  orderQty: {
+  orderQuantity: {
     type: Number,
     required: [true, 'Order Qty is required!'],
     min: [1, 'Order Qty is too short!'],
@@ -52,33 +52,21 @@ const OrderSchema = new mongoose.Schema({
     required: [true, 'Total Payout for Vendor is required!'],
     min: [0, 'Total Payout for Vendor is too short!'],
   },
-  status: {
+  orderNote: {
     type: String,
-    enum: {
-      values: ['pending', 'delivered'],
-      message: '{VALUE} is an invalid status!'
-    },
-    default: 'pending',
-    lowercase: true,
   },
-  pickUpType: {
-    type: String,
-    enum: {
-      values: ['pick-up', 'delivery'],
-      message: '{VALUE} is an invalid pick-up type!'
-    },
-    required: [true, 'Pick-Up Type is required!'],
-    lowercase: true,
-  },
-  totalPaidToVendor: {
-    type: Number,
-    required: [true, 'Total Amount Paid to Vendor is required!'],
-    min: [0, 'Total Amount Paid to Vendor is too short!'],
-  },
-  totalPaidToRetailer: {
-    type: Number,
-    required: [true, 'Total Amount Paid to Retailer is required!'],
-    min: [0, 'Total Amount Paid to Retailer is too short!'],
+  orderChat: {
+    type: [
+      {
+        sender: {
+          type: Schema.Types.ObjectId,
+          ref: User,
+          required: [true, 'Retailer ID is required!'],
+        },
+        comment: {type:String},
+        voiceNote: {type:String}
+      }
+    ]
   },
   orderDate: {
     type: Date,
@@ -95,6 +83,44 @@ const OrderSchema = new mongoose.Schema({
   deliverDate: {
     type: Date,
     required: [true, 'Deliver Date is required!'],
+  },
+  totalPaidToVendor: {
+    type: Number,
+    required: [true, 'Total Amount Paid to Vendor is required!'],
+    min: [0, 'Total Amount Paid to Vendor is too short!'],
+  },
+  totalPaidByRetailer: {
+    type: Number,
+    required: [true, 'Total Amount Paid to Retailer is required!'],
+    min: [0, 'Total Amount Paid to Retailer is too short!'],
+  },
+  pickUpType: {
+    type: String,
+    enum: {
+      values: ['pick-up', 'delivery'],
+      message: '{VALUE} is an invalid pick-up type!'
+    },
+    required: [true, 'Pick-Up Type is required!'],
+    lowercase: true,
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: User,
+    required: [true, 'User ID is required!'],
+  },
+  updatedBy: {
+    type: Schema.Types.ObjectId,
+    ref: User,
+    required: [true, 'User ID is required!'],
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ['pending', 'delivered'],
+      message: '{VALUE} is an invalid status!'
+    },
+    default: 'pending',
+    lowercase: true,
   },
 }, { timestamps: true })
 
